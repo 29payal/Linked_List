78a39eec7877845866b888cd0b491c7ba8465440
Payal Sharma <https://github.com/29payal>
2022-09-13 17:30:01 +0530

    [Payal] UC4-InsertElement

--- Linkedlist/Program.cs
+++ Linkedlist/Program.cs
@@ -15,7 +15,7 @@ namespace Linkedlist
             Console.WriteLine("Print the elements of simple linked list :");
             simpleLinkedList.Add();
 
-            //NODE LINKED LIST
+            //NODE LINKED LIST// reverse order
             Console.WriteLine("#############################################");
             CustomlinkedList customlinkedList = new CustomlinkedList();
             Console.WriteLine("Elements in reverse order :");
@@ -24,6 +24,7 @@ namespace Linkedlist
             customlinkedList.AddFirst(56);
             customlinkedList.Display();
 
+            //moving order
             Console.WriteLine("##################################################");
             CustomLinkedList customLinkedList = new CustomLinkedList();
             Console.WriteLine("Print the elements of the list in moving order :");
@@ -31,7 +32,19 @@ namespace Linkedlist
             customLinkedList.AddEnd(30);
             customLinkedList.AddEnd(70);
             customLinkedList.Display();
-           
+
+            //insert
+            Console.WriteLine("###############################################");
+            InsertElement insertElement = new InsertElement();
+            Console.WriteLine("Elements before insert");
+            insertElement.AddEnd(56);
+            insertElement.AddEnd(70);
+            insertElement.Display();
+            Console.WriteLine("___________________________________");
+
+            Console.WriteLine("Elements after insert");
+            insertElement.InsertInBetween(30, 2);
+            insertElement.Display();
             Console.ReadLine();
         }
     }
