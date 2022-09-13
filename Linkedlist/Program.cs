c86fe0bbd54fa7be35c93e99a1a44bdfb439b7db
29payal <111033408+29payal@users.noreply.github.com>
2022-09-13 19:02:57 +0530

    Update Program.cs

--- Linkedlist/Program.cs
+++ Linkedlist/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,29 +10,7 @@ namespace Linkedlist
     {
         static void Main(string[] args)
         {
-            //SIMPLE LINKED LIST
-            SimpleLinkedList simpleLinkedList = new SimpleLinkedList();
-            Console.WriteLine("Print the elements of simple linked list :");
-            simpleLinkedList.Add();
-
-            //NODE LINKED LIST// reverse order
-            Console.WriteLine("#############################################");
-            CustomlinkedList customlinkedList = new CustomlinkedList();
-            Console.WriteLine("Elements in reverse order :");
-            customlinkedList.AddFirst(70);
-            customlinkedList.AddFirst(30);
-            customlinkedList.AddFirst(56);
-            customlinkedList.Display();
-
-            //moving order
-            Console.WriteLine("##################################################");
-            CustomLinkedList customLinkedList = new CustomLinkedList();
-            Console.WriteLine("Print the elements of the list in moving order :");
-            customLinkedList.AddEnd(56);
-            customLinkedList.AddEnd(30);
-            customLinkedList.AddEnd(70);
-            customLinkedList.Display();
-
+            
             //insert
             Console.WriteLine("###############################################");
             InsertElement insertElement = new InsertElement();
