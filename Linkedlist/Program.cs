5d555fcc247d93c8f39bfd59dbcb5e464d77dc06
29payal <111033408+29payal@users.noreply.github.com>
2022-09-13 19:04:03 +0530

    Update Program.cs

--- Linkedlist/Program.cs
+++ Linkedlist/Program.cs
@@ -10,42 +10,7 @@ namespace Linkedlist
     {
         static void Main(string[] args)
         {
-            //SIMPLE LINKED LIST
-            SimpleLinkedList simpleLinkedList = new SimpleLinkedList();
-            Console.WriteLine("Print the elements of simple linked list :");
-            simpleLinkedList.Add();
-
-            //NODE LINKED LIST// reverse order
-            Console.WriteLine("#############################################");
-            CustomlinkedList customlinkedList = new CustomlinkedList();
-            Console.WriteLine("Elements in reverse order :");
-            customlinkedList.AddFirst(70);
-            customlinkedList.AddFirst(30);
-            customlinkedList.AddFirst(56);
-            customlinkedList.Display();
-
-            //moving order
-            Console.WriteLine("##################################################");
-            CustomLinkedList customLinkedList = new CustomLinkedList();
-            Console.WriteLine("Print the elements of the list in moving order :");
-            customLinkedList.AddEnd(56);
-            customLinkedList.AddEnd(30);
-            customLinkedList.AddEnd(70);
-            customLinkedList.Display();
-
-            //insert
-            Console.WriteLine("###############################################");
-            InsertElement insertElement = new InsertElement();
-            Console.WriteLine("Elements before insert");
-            insertElement.AddEnd(56);
-            insertElement.AddEnd(70);
-            insertElement.Display();
-            Console.WriteLine("___________________________________");
-
-            Console.WriteLine("Elements after insert");
-            insertElement.InsertInBetween(30, 2);
-            insertElement.Display();
-
+            
             //Delete first element
             Console.WriteLine("###################################################");
             DeleteFirstElement deleteFirstElement = new DeleteFirstElement();
@@ -54,6 +19,7 @@ namespace Linkedlist
             deleteFirstElement.AddEnd(30);
             deleteFirstElement.AddEnd(70);
             deleteFirstElement.Display();
+            
             Console.WriteLine("___________________________________________________");
             Console.WriteLine("After delete the element the list is :");
             deleteFirstElement.DeleteFirstNode();
